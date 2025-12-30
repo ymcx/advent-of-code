5d3282bed0cae1ffc1fe895bd051d455d3077a24
ymcx <89810988+ymcx@users.noreply.github.com>
2025-12-30 16:49:17 +0200

    2022, day 7

---
+++ 2022/07a.rs
@@ -0,0 +1,59 @@
+use std::{collections::HashMap, io, path::PathBuf};
+
+fn cd(line: &str, directory: &PathBuf) -> PathBuf {
+    let mut directory = directory.clone();
+
+    let next = line.split(' ').last().unwrap();
+    if next == ".." {
+        directory.pop();
+    } else {
+        directory.push(PathBuf::from(next));
+    }
+
+    directory
+}
+
+fn ls(line: &str, directory: &PathBuf) -> (PathBuf, u32) {
+    let (size, file) = line.split_once(' ').unwrap();
+
+    let mut directory = directory.clone();
+    directory.push(file);
+
+    let size = size.parse().unwrap_or_default();
+    (directory, size)
+}
+
+fn main() {
+    let lines: Vec<String> = io::stdin().lines().map(|i| i.unwrap()).collect();
+    let mut directory = PathBuf::from("/");
+    let mut files: HashMap<PathBuf, u32> = HashMap::new();
+
+    for line in lines {
+        if line.starts_with("$ cd") {
+            directory = cd(&line, &directory);
+        } else if !line.starts_with("$") {
+            let (file, size) = ls(&line, &directory);
+            files.insert(file, size);
+        }
+    }
+
+    let mut result = 0;
+    for (directory, &score) in files.iter() {
+        if score != 0 {
+            continue;
+        }
+
+        let mut sum = 0;
+        for (d, &s) in files.iter() {
+            if d.starts_with(directory) {
+                sum += s;
+            }
+        }
+
+        if sum <= 100000 {
+            result += sum;
+        }
+    }
+
+    println!("{}", result);
+}
